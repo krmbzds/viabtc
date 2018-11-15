391f8b8cee4f77e9dd533c8b99dceed513061fab
Kerem Bozdas <krmbzds.github@gmail.com>
2018-11-15 11:10:34 +0300

    Add custom error base class

--- lib/viabtc/error.rb
+++ lib/viabtc/error.rb
@@ -1,13 +1,24 @@
 module ViaBTC
   module Error
-    # This gem will only raise errors that originate from the exchange server.
+    # Base Error Class
+    class ExchangeError < StandardError
+      attr_reader :response
+      def initialize(response)
+        @code = response['error']['code']
+        @message = response['error']['message']
+        @response = response
+      end
+    end
 
-    ExchangeError      = Class.new(StandardError)  # Base Error Class
-
+    # ViaBTC Exchange Server General Error Codes
     InvalidArgument    = Class.new(ExchangeError)  # 1: Invalid Argument
     InternalError      = Class.new(ExchangeError)  # 2: Internal Error
     ServiceUnavailable = Class.new(ExchangeError)  # 3: Service Unavailable
     MethodNotFound     = Class.new(ExchangeError)  # 4: Method Not Found
     ServiceTimeout     = Class.new(ExchangeError)  # 5: Service Timeout
+
+    ViaBTCError        = Class.new(StandardError)
+    Configuration      = Class.new(ViaBTCError)
+    InvalidParameter   = Class.new(ViaBTCError)
   end
 end
